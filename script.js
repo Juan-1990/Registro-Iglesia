e65bdb8f5d12ab58a337fbe59d9984520a5357f2
Juan <jcvr081990@gmail.com>
2025-04-28 12:37:15 -0500

    mejora

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
       edad: document.getElementById('edad').value
     };
 
-    fetch('https://script.google.com/macros/s/AKfycbyLPZ4a9tLSJV8N_exhD4Fl_hjB4qBdcNQ9paMwrllT17Yi8pSm6Y6fQDQBO5zyT7Ts/exec', {
+    fetch(https://script.google.com/macros/s/AKfycbwZweBVfm-0eA6GL87Z6CBCbrVJNuhSXc8hz1V6XwMAgPFJ8UIov1N68fdBh7mTNUOg/exec, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
